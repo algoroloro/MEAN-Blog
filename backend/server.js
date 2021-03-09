c5a11d28dcacee71719665cefde9a8b3792d156f
algoroloro <algoro9697@gmail.com>
2021-03-09 21:13:31 +0100

    CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 require('dotenv').config();
 
+const cors = require('./middlewares/cors.js');
 require('./config/dbconnect.js');
 
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors);
 
 app.listen(PORT, () => {
     console.log('---> SERVER RUNNING ON PORT ' + PORT);
